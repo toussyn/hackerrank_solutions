bdff0223051f46a769d950ca8aa0044199ae7a0e
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2023-03-13 15:52:34 +0100

    Create ubers_third_transaction.sql

---
+++ sql/datalemur/ubers_third_transaction.sql
@@ -0,0 +1,15 @@
+WITH trans_num AS (
+  SELECT 
+    user_id, 
+    spend, 
+    transaction_date, 
+    ROW_NUMBER() OVER (
+      PARTITION BY user_id ORDER BY transaction_date) AS row_num 
+  FROM transactions)
+ 
+SELECT 
+  user_id, 
+  spend, 
+  transaction_date 
+FROM trans_num 
+WHERE row_num = 3;
