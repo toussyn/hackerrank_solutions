d149232a55f71e21210609b5288e75652e6760c9
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2023-03-14 06:30:36 +0100

    Create sending_vs_opening_snaps.sql

---
+++ sql/datalemur/sending_vs_opening_snaps.sql
@@ -0,0 +1,13 @@
+SELECT 
+  age.age_bucket, 
+  ROUND(100.0 * 
+    SUM(activities.time_spent) FILTER (WHERE activities.activity_type = 'send')/
+      SUM(activities.time_spent),2) AS send_perc, 
+  ROUND(100.0 * 
+    SUM(activities.time_spent) FILTER (WHERE activities.activity_type = 'open')/
+      SUM(activities.time_spent),2) AS open_perc
+FROM activities
+INNER JOIN age_breakdown AS age 
+  ON activities.user_id = age.user_id 
+WHERE activities.activity_type IN ('send', 'open') 
+GROUP BY age.age_bucket;
